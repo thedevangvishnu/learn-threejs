bdad224b64399e7417d3a852260126ccc4e06d56
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-21 19:38:37 +0530

    Solve the resizing issue

--- section-2-threejs-fundamentals/camera-and-renderloop/src/script.js
+++ section-2-threejs-fundamentals/camera-and-renderloop/src/script.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 
 const cubeMeshGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMeshMaterial = new THREE.MeshBasicMaterial({
-  color: "rgb(41, 242, 199)",
+  color: "rgb(66, 245, 206)",
 });
 const cubeMesh = new THREE.Mesh(cubeMeshGeometry, cubeMeshMaterial);
 
@@ -14,19 +14,27 @@ scene.add(cubeMesh);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  1,
-  20
+  0.1,
+  30
 );
-camera.position.z = 5;
 
+camera.position.z = 5;
 scene.add(camera);
 
 const canvas = document.getElementById("threejs");
-const renderer = new THREE.WebGLRenderer({ canvas });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 const renderLoop = () => {
   controls.update();
