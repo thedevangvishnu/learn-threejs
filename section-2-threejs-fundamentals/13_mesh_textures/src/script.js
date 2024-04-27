02c576b371480d1185f78d6aa6eca0995009a02e
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-27 16:58:58 +0530

    Use texture on a material for multiple meshes
    Learn to load a texture and then use it for a material.

--- section-2-threejs-fundamentals/13_mesh_textures/src/script.js
+++ section-2-threejs-fundamentals/13_mesh_textures/src/script.js
@@ -12,26 +12,44 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5, 0.15, 100, 16);
 const planeGeometry = new THREE.PlaneGeometry(1, 1);
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32, 32);
+
+// initialize the texture
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load(
+  "/textures/whispy-grass-meadow-bl/wispy-grass-meadow_albedo.png"
+);
 
 // initialize the material
-const material = new THREE.MeshBasicMaterial();
+const material = new THREE.MeshPhongMaterial();
+material.side = THREE.DoubleSide;
+material.map = texture;
+material.color = new THREE.Color("yellow");
 
 // initialize the mesh
 const cube = new THREE.Mesh(geometry, material);
 
 const knot = new THREE.Mesh(torusKnotGeometry, material);
-knot.position.x = 1.5;
+knot.position.x = 2;
 
 const plane = new THREE.Mesh(planeGeometry, material);
-plane.position.x = -1.5;
 
-// add the mesh to the scene
-scene.add(cube);
-scene.add(knot);
-scene.add(plane);
+plane.position.x = -2;
+
+const sphere = new THREE.Mesh(sphereGeometry, material);
+sphere.position.y = 2;
+
+const cylinder = new THREE.Mesh(cylinderGeometry, material);
+cylinder.position.y = -2;
+
+// add the mesh to the scene/group
+const group = new THREE.Group();
+group.add(cube, knot, plane, sphere, cylinder);
+scene.add(group);
 
 // initialize the light
-const light = new THREE.AmbientLight(0xffffff, 0.4);
+const light = new THREE.AmbientLight(0xffffff, 0.7);
 scene.add(light);
 
 const pointLight = new THREE.PointLight(0xffffff, 1.2);
@@ -68,6 +86,12 @@ window.addEventListener("resize", () => {
 
 // render the scene
 const renderloop = () => {
+  group.children.forEach((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.rotation.y += 0.02;
+    }
+  });
+
   controls.update();
   renderer.render(scene, camera);
   window.requestAnimationFrame(renderloop);
