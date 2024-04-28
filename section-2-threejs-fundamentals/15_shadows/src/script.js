076b19608d8ab8fb7d09003dcb0c110a376d6208
Devang Vishnu <thedevangvishnu@gmail.com>
2024-04-28 17:52:06 +0530

    Learn about shadows, their types and how to tweak them

--- section-2-threejs-fundamentals/15_shadows/src/script.js
+++ section-2-threejs-fundamentals/15_shadows/src/script.js
@@ -7,7 +7,7 @@ const scene = new THREE.Scene();
 const material = new THREE.MeshStandardMaterial({
   color: 0xffffff,
   metalness: 0.0,
-  roughness: 0.,
+  roughness: 0.0,
 });
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -17,40 +17,77 @@ const circleGeometry = new THREE.CircleGeometry(0.5, 32);
 
 const box = new THREE.Mesh(boxGeometry, material);
 box.position.x = -2;
+box.castShadow = true;
+box.receiveShadow = true;
 
 const sphere = new THREE.Mesh(sphereGeometry, material);
 sphere.position.x = 0;
+sphere.castShadow = true;
+sphere.receiveShadow = true;
 
 const torusKnot = new THREE.Mesh(torusKnotGeometry, material);
 torusKnot.position.x = 2;
+torusKnot.castShadow = true;
 
 const circle = new THREE.Mesh(circleGeometry, material);
 circle.scale.setScalar(20);
 circle.position.y = -2;
 circle.rotation.x = -Math.PI / 2;
+circle.receiveShadow = true;
 
 scene.add(box, sphere, torusKnot, circle);
 
-const directionalLight = new THREE.DirectionalLight(0xff0000, 0.8);
+const directionalLight = new THREE.DirectionalLight(0xff0000, 1);
 directionalLight.position.set(3, 10, 15);
 scene.add(directionalLight);
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight
+directionalLight.castShadow = true;
+
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+directionalLight.shadow.radius = 5;
+
+directionalLight.shadow.camera.near = 2;
+directionalLight.shadow.camera.far = 100;
+
+directionalLight.shadow.camera.left = -3;
+directionalLight.shadow.camera.right = 3;
+directionalLight.shadow.camera.top = 3;
+directionalLight.shadow.camera.bottom = -3;
+
+// const directionalLightHelper = new THREE.DirectionalLightHelper(
+//   directionalLight
+// );
+// scene.add(directionalLightHelper);
+
+const directionLightCameraHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
 );
-scene.add(directionalLightHelper);
+scene.add(directionLightCameraHelper);
 
 const pointLight = new THREE.PointLight(0x00ff00, 0.6);
 pointLight.position.set(-3, 2, -3);
 scene.add(pointLight);
+// pointLight.castShadow = true;
 const pointLightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(pointLightHelper);
 
-const spotLight = new THREE.SpotLight(0x0000ff, 1, 30, Math.PI * 0.1);
+const spotLight = new THREE.SpotLight(0x0000ff, 3, 30, Math.PI * 0.1, 0.2);
 spotLight.position.set(8, 4, 4);
 spotLight.target.position.set(0, -1, 0);
 scene.add(spotLight);
-const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-scene.add(spotLightHelper);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.radius = 5;
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 40;
+spotLight.shadow.camera.fov = 20;
+
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+// scene.add(spotLightHelper);
+
+// const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+// scene.add(spotLightCameraHelper);
 
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -65,6 +102,7 @@ const canvas = document.querySelector("canvas.threejs");
 const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
